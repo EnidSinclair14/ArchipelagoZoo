5a41e7000821b6d53849beca74e685c5974403b1
Endelif <92841501+idkwhyi@users.noreply.github.com>
2024-07-20 20:18:38 +0700

    Setup react router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,25 @@
 import './App.css';
-import './styles/lexend.css' // Lexend Font Family
-import './styles/poppins.css' // Poppins Font Family
+import './styles/lexend.css'; // Lexend Font Family
+import './styles/poppins.css'; // Poppins Font Family
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
 // Pages
+import HomePage from './pages/Home/HomePage';
+// import AboutPage from './pages/AboutPage';
+// import NotFoundPage from './pages/NotFoundPage';
 
 function App() {
   return (
-    <div className="App ">
-      Hello World!
-    </div>
+    <Router>
+      {/* <Header /> */}
+      {/* <div> */}
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        {/* <Route path="/about" element={<AboutPage />} /> */}
+        {/* <Route path="*" element={<NotFoundPage />} /> */}
+      </Routes>
+      {/* </div> */}
+    </Router>
   );
 }
 
